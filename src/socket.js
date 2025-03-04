5db2cf5262b139b1556fab002817de68b669c7ec
rajsrimann15 <rajsrimannwork@gmail.com>
2025-03-04 19:42:29 +0530

    initial

--- src/socket.js
+++ src/socket.js
@@ -3,3 +3,5 @@ import { io } from "socket.io-client";
 
 const backendLink = import.meta.env.VITE_BACKEND_LINK;
 export const socket = io(backendLink, { autoConnect: true }); 
+
+console.log("hello");
