5e21af3e352f37388a7a9998ca30133eebad49ea
rajsrimann15 <rajsrimannwork@gmail.com>
2025-03-06 15:06:20 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
 
   return (
     <Router>
-      <NavigationGuard />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
